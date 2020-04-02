b95c8a5bfd3b4a1497de9532ba4086f8b1418912
Wenkai Zheng <zheng.wenk@husky.neu.edu>
2020-04-02 17:25:23 -0400

    fix typo

--- coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/CoronavariusTrackerApplication.java
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/CoronavariusTrackerApplication.java
@@ -1,4 +1,4 @@
-package io.javabrains.coronavariustracker;
+package io.javabrains.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- coronavirus-tracker/src/test/java/io/javabrains/coronavirustracker/CoronavariusTrackerApplicationTests.java
+++ coronavirus-tracker/src/test/java/io/javabrains/coronavirustracker/CoronavariusTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.javabrains.coronavariustracker;
+package io.javabrains.coronavirustracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
