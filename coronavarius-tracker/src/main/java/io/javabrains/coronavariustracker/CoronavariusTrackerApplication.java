f0ccf7d26557eda530f377850d4e0cf85e70e151
Wenkai Zheng <zheng.wenk@husky.neu.edu>
2020-04-02 17:11:19 -0400

    initial stage

---
+++ coronavarius-tracker/src/main/java/io/javabrains/coronavariustracker/CoronavariusTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.coronavariustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavariusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavariusTrackerApplication.class, args);
+	}
+
+}
---
+++ coronavarius-tracker/src/test/java/io/javabrains/coronavariustracker/CoronavariusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.coronavariustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavariusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
